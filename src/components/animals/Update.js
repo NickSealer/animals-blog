a7b72a699e1ac58c7796150f7729479c2ac2afa7
NickSealer <nickfontechav@gmail.com>
2024-04-13 16:59:32 -0500

    Update animal

--- src/components/animals/Update.js
+++ src/components/animals/Update.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
-import api from '../../animalsMock'
+import React, { useEffect, useState } from 'react'
 import { Form } from '../../routes/animals/Form'
 import { useNavigate, useParams } from 'react-router-dom'
+import db from '../../firebase'
+import { Loading } from '../Loading'
 
 export const Update = () => {
   const { id } = useParams('id')
-  const animal = api[id]
-  const [animalName, setAnimalName] = useState(animal.name)
-  const [photo, setPhoto] = useState(animal.photo)
-  const [description, setDescription] = useState(animal.description)
+  const [animal, setAnimal] = useState()
+  const [animalName, setAnimalName] = useState(animal?.name)
+  const [photo, setPhoto] = useState(animal?.photo)
+  const [description, setDescription] = useState(animal?.description)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    try {
+      const getAnimal = async () => {
+        const animalRef = await db.collection('animals').doc(id).get();
+        if (animalRef.exists) {
+          let result = { id, ...animalRef.data() }
+          setAnimal(result)
+        } else {
+          alert('No animal found.')
+        }
+      }
+      getAnimal()
+    } catch (error) {
+      alert(`Error: ${error}`);
+    }
+  }, [id])
 
   const handleInput = (e, inputField) => {
     switch (inputField) {
@@ -25,12 +43,30 @@ export const Update = () => {
     }
   }
 
-  const handleSubmit = (e) => {
+  const updateAnimal = async (e) => {
     e.preventDefault()
-    alert(`${animal.name} updated!`)
-    navigate(`/animals/${animal.id}`)
+    let animlaUpdated = {
+      name: animalName || animal.name,
+      photo: photo || animal.photo,
+      description: description || animal.description
+    }
+
+    try {
+      await db.collection('animals').doc(animal.id).update(animlaUpdated)
+      alert(`${animal.name} updated!`)
+      navigate(`/animals/${animal.id}`)
+    } catch (error) {
+      alert(`Error: ${error}`);
+    }
   }
+
   return (
-    <Form animal={animal} handleInput={handleInput} handleSubmit={handleSubmit}/>
+    <>
+      {animal ? (
+        <Form animal={animal} handleInput={handleInput} handleSubmit={updateAnimal} />
+      ) : (
+        <Loading />
+      )}
+    </>
   )
 }
