2ad950e5774bff6146cc03acbae4cdfc2900a2d6
NickSealer <nickfontechav@gmail.com>
2024-04-13 15:23:20 -0500

    List all animals

--- src/components/animals/Animals.js
+++ src/components/animals/Animals.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import api from '../../animalsMock'
+import React, { useEffect, useState } from 'react'
 import { Card } from '../Card';
+import db from '../../firebase';
+import { Loading } from '../Loading';
 
 export const Animals = () => {
-  const animals = api;
+  const [animals, setAnimals] = useState([]);
+  const [status, setStatus] = useState(0);
+
+  useEffect(() => {
+    try {
+      const getAnimals = async () => {
+        const response = await db.collection('animals').get();
+        response.forEach((animal) => {
+          let data = animal.data();
+          let id = animal.id;
+          let result = { id: id, ...data }
+          setAnimals((animals) => [...animals, result])
+          setStatus(200)
+        })
+      }
+      getAnimals()
+    } catch (error) {
+      alert(`Error: ${error}`);
+    }
+  }, [])
 
   return (
     <>
-      {
-        animals.map((animal) => {
+      {status === 200 ? (
+        animals.map((animal, index) => {
           return (
-            <Card animal={animal} key={animal.id}/>
+            <Card animal={animal} key={index} />
           )
         })
+      ) : (
+        <Loading />
+      )
       }
     </>
   )
