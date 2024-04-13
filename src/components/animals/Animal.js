e862ede6537684dfa55d0ca867b2982945ad946d
NickSealer <nickfontechav@gmail.com>
2024-04-13 15:23:51 -0500

    Get an animal

--- src/components/animals/Animal.js
+++ src/components/animals/Animal.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import api from '../../animalsMock'
+import React, { useEffect, useState } from 'react'
 import { Details } from '../Details'
 import { useParams } from 'react-router-dom'
+import db from '../../firebase'
+import { Loading } from '../Loading'
 
 export const Animal = () => {
-  const {id} = useParams('id')
-  const animal = api[id]
+  const { id } = useParams('id')
+  const [animal, setAnimal] = useState()
+
+  useEffect(() => {
+    try {
+      const getAnimal = async () => {
+        const animalRef = await db.collection('animals').doc(id).get();
+        if (animalRef.exists) {
+          let result = { id, ...animalRef.data() }
+          setAnimal(result)
+        } else {
+          alert('No animal found.')
+        }
+      }
+      getAnimal()
+    } catch (error) {
+      alert(`Error: ${error}`);
+    }
+  }, [id])
 
   return (
     <>
-      <Details animal={animal}/>
+      {animal ? (
+        <Details animal={animal} />
+      ) : (
+        <Loading />
+      )}
     </>
   )
 }
